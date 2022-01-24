69539b4f19181c77eefa36c20cdd66f912e76468
Andreas Börjesson <borjesson.andreas@gmail.com>
2022-01-24 06:11:12 +0100

    Metatags component added.

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,6 +1,9 @@
+import Metatags from "../../components/Metatags";
+
 export default function AdminEditPostPage({}) {
   return (
     <main>
+      <Metatags title="Admin page" />
       <h1>Admin Edit Post Page</h1>
     </main>
   );

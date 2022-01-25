c6a7cd6a25fcfd61fb41f42a9e410e08aeb108c0
Andreas Börjesson <borjesson.andreas@gmail.com>
2022-01-25 06:20:59 +0100

    Now possible to heart/like an article.

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,14 @@
 import styles from "../../styles/Post.module.css";
 import PostContent from "../../components/PostContent";
+import AuthCheck from "../../components/AuthCheck";
+import HeartButton from "../../components/HeartButton";
+import { UserContext } from "../../lib/context";
+
+import Link from "next/link";
+
 import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useContext } from "react";
 
 export async function getStaticProps({ params }) {
   const { username, slug } = params;
@@ -50,6 +57,8 @@ export default function Post(props) {
 
   const post = realtimePost || props.post;
 
+  const { user: currentUser } = useContext(UserContext);
+
   return (
     <main className={styles.container}>
       <section>
@@ -60,6 +69,22 @@ export default function Post(props) {
         <p>
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
+
+        <AuthCheck
+          fallback={
+            <Link href="/enter">
+              <button>💗 Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
+
+        {currentUser?.uid === post.uid && (
+          <Link href={`/admin/${post.slug}`}>
+            <button className="btn-blue">Edit Post</button>
+          </Link>
+        )}
       </aside>
     </main>
   );
